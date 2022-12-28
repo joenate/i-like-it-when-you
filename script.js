d0161d2643956ee9511ea22a79b3e5feb2192445
Jonathan Pierre <102133146+joenate@users.noreply.github.com>
2022-12-27 20:40:45 -0500

    added autoplay attribute to iframe

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var c = 0;
 
-var when = ["listen to Gillian Welch", "turn the radio down", "put on Liz Phair", "lean back in your chair", "turn the dial low", "fall fast asleep in the back of my car", '<iframe width="560" height="315" src="https://www.youtube.com/embed/d5NCuzksTVU?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'];
+var when = ["listen to Gillian Welch", "turn the radio down", "put on Liz Phair", "lean back in your chair", "turn the dial low", "fall fast asleep in the back of my car", '<iframe width="560" height="315" src="https://www.youtube.com/embed/d5NCuzksTVU?controls=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'];
 
 
 // old
